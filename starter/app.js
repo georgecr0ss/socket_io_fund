c034fe0f0b1ce79d5907e503b2edf5a0c16b4fe5
georgecr0ss <george.krystew@gmail.com>
2016-09-02 18:50:03 +0300

    basic socket connection

--- starter/app.js
+++ starter/app.js
@@ -8,6 +8,17 @@ var port = 8866;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on('connection', function(socket){
+	console.log('new connection made');
+
+	socket.emit('message-from-server', {
+		greeting: 'Hello Server'
+	});
+
+	socket.on('message-from-client', function (msg){
+		console.log(msg);
+	})
+})
 server.listen(port,function(){
 	console.log("listening on port %s", port);
 });
